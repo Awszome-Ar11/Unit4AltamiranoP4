c8aa881486d09f034cade471188021cdb98bc53b
Awszome-Ar11 <743241@student.dusd.net>
2026-04-22 11:45:22 -0700

    finisheds unnit 4
    finished unit 4 - mising bonus features
    started chgaallenge 4

--- P4AltamiranoUnit/Assets/Scripts/SpawnManager.cs
+++ P4AltamiranoUnit/Assets/Scripts/SpawnManager.cs
@@ -2,21 +2,40 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+
     public GameObject enemyPrefab;
     private float spawnRange = 9;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
     
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i=0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
     private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
